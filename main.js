5172af56cb46f8505478d12fd6936cb9b93b1958
NishantCoding77 <110609704+NishantCoding77@users.noreply.github.com>
2023-11-10 16:54:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,15 +2,10 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+leftWristScore = 0;
+SongStatus = "";
+SongStatus2= "";
 
-function setup(){
-    canvas = createCanvas(600,530);
-    canvas.center();
-
-    posenet=ml5.poseNet(video,modelLoaded);
-    posenet.on('pose',gotPoses);
-
-}
 
 song = ""
 song1 = ""
@@ -23,13 +18,30 @@ function preload(){
 function setup(){
     canvas= createCanvas(600,500);
     canvas.center();
-
+   
     video = createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 
 function draw(){
   image(video,0,0,600,500);
+  fill("#0000FF");
+  stroke("#0000FF");
+  songStatus2=song1.isPlaying();
+  songStatus=song.isPlaying();
+  if(leftWristScore>0.2){
+    circle(leftWristX,leftWristY,20)
+    song1.stop();
+    if(SongStatus2 == false ){
+        song.play();
+        song.setVolume(1);
+        song.rate(0.8);
+        document.getElementById("songname").innerHTML = "Playing Harry Potter OST";
+    }
+  }
+  
 }
 
 function modelLoaded(){
@@ -39,6 +51,7 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        leftWristScore = results[0].pose.keypoints[9].score;
         leftWristX= results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY=" + leftWristY )
